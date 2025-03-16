8265e42da72123724e87fca3784ac3c3f974ae33
Ayrton Lins <ayrton.lins@gmail.com>
2025-03-16 01:46:23 -0300

    Teste commit Projeto Amigo Secreto

--- app.js
+++ app.js
@@ -1,1 +1,2 @@
+//Projeto Challenge Amigo Secreto
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
