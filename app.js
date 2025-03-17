dc53dc78c619a1be7976a403cb8a7f75d8e1eecc
Ayrton Lins <ayrton.lins@gmail.com>
2025-03-17 00:44:24 -0300

    Implementação de funções

--- app.js
+++ app.js
@@ -1,2 +1,38 @@
 //Projeto Challenge Amigo Secreto 
 
+let amigos = []
+
+
+function adicionarAmigo() {
+    
+    // Capturar o valor do campo de entrada
+    let amigo = recuperarValorPeloId('amigo');
+
+    // Validação para garantir que o campo não esteja vazio
+    if (amigo === "") {
+        alert("Por favor, insira um nome.");
+    } else {
+        adionarValorListaAmigos(amigo);    
+    }
+
+     
+ 
+}
+
+
+// Recuperar o valor do campo pelo ID informado
+function recuperarValorPeloId(id) {
+      
+      return document.getElementById(id).value;   
+}
+
+// Adicionar o nome do amigo a lista de amigos
+function adionarValorListaAmigos(amigo) {
+    
+    amigos.push(amigo);
+}
+
+
+
+
+
