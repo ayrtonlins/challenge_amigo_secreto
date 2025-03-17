536ffd6b0f0e907ece4bbb8eebd4b1bfeb6d5286
Ayrton Lins <ayrton.lins@gmail.com>
2025-03-17 15:01:41 -0300

    Adicionando a funcção limpar campo

--- app.js
+++ app.js
@@ -6,24 +6,24 @@ let amigos = []
 function adicionarAmigo() {
     
     // Capturar o valor do campo de entrada
-    let amigo = recuperarValorPeloId('amigo');
+    let amigo = recuperarValorPeloId('amigo');  
 
     // Validação para garantir que o campo não esteja vazio
     if (amigo === "") {
         alert("Por favor, insira um nome.");
     } else {
-        adionarValorListaAmigos(amigo);    
+        adionarValorListaAmigos(amigo);
+        limparCampo('amigo');
     }
-
-     
- 
+        alert(amigos);
+      
 }
 
 
 // Recuperar o valor do campo pelo ID informado
 function recuperarValorPeloId(id) {
       
-      return document.getElementById(id).value;   
+      return document.getElementById(id).value.trim();   
 }
 
 // Adicionar o nome do amigo a lista de amigos
@@ -32,7 +32,10 @@ function adionarValorListaAmigos(amigo) {
     amigos.push(amigo);
 }
 
-
+// Limpar o campo de entrada
+function limparCampo(id){
+    document.getElementById(id).value = '';
+}
 
 
 
